2a7db05e1470e3748e6d02e84f33306ee7127981
Prayag Joshi <prayagjoshi263@gmail.com>
2024-03-18 12:38:07 +0530

    running graphql on express server

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-var { graphql, buildSchema } = require("graphql");
+var express = require("express");
+var { createHandler } = require("graphql-http/lib/use/express");
+var { buildSchema } = require("graphql");
+var { ruruHTML } = require("ruru/server");
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -8,8 +11,8 @@ var schema = buildSchema(`
   }
 `);
 
-// The rootValue provides a resolver function for each API endpoint
-var rootValue = {
+// The root provides a resolver function for each API endpoint
+var root = {
   hello: () => {
     return "Hello world!";
   },
@@ -18,11 +21,24 @@ var rootValue = {
   },
 };
 
-// Run the GraphQL query '{ hello }' and print out the response
-graphql({
-  schema,
-  source: "{ hello, age }",
-  rootValue,
-}).then((response) => {
-  console.log(response);
+var app = express();
+
+// Create and use the GraphQL handler.
+app.all(
+  "/graphql",
+  createHandler({
+    schema: schema,
+    rootValue: root,
+  })
+);
+
+// Serve the GraphiQL IDE.
+app.get("/", (_req, res) => {
+  res.type("html");
+  res.end(ruruHTML({ endpoint: "/graphql" }));
 });
+
+// Start the server at port
+app.listen(4000);
+console.log(`Running a GraphQL API server at http://localhost:4000/graphql
+Running a GraphQL API server at http://localhost:4000/graphql?query={hello,age}`);
