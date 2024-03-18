9fc5c0d25375e417d3f46532912fdcbf37ab4c01
Prayag Joshi <prayagjoshi263@gmail.com>
2024-03-18 16:49:01 +0530

    GraphQL-yoga setup

--- server.js
+++ server.js
@@ -1,52 +1,26 @@
-var express = require("express");
-var { createHandler } = require("graphql-http/lib/use/express");
-var { buildSchema } = require("graphql");
-var { ruruHTML } = require("ruru/server");
+import express from "express";
+import ruruHTML from "ruru/server";
+import { createSchema, createYoga } from "graphql-yoga";
 
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello(name: String!): String
-    age: Int
-    weight: Float
-    isOver18: Boolean
-    hobbies: [String]
-    quoteOfTheDay: String
-  }
-`);
-
-// The root provides a resolver function for each API endpoint
-var root = {
-  hello: (args) => {
-    return `Hello ${args.name}!`;
-  },
-  age: () => {
-    return 23;
-  },
-  weight: () => {
-    return 70.55;
-  },
-  isOver18: () => {
-    return true;
-  },
-  hobbies: () => {
-    return ["running", "swimming", "dancing"];
-  },
-  quoteOfTheDay: () => {
-    return Math.random() < 0.5 ? "Take it easy" : "Salvation lies within";
-  },
-};
+const yoga = createYoga({
+  schema: createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        hello: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => "Hello from Yoga!",
+      },
+    },
+  }),
+});
 
 var app = express();
 
 // Create and use the GraphQL handler.
-app.all(
-  "/graphql",
-  createHandler({
-    schema: schema,
-    rootValue: root,
-  })
-);
+app.all("/graphql", yoga);
 
 // Serve the GraphiQL IDE.
 app.get("/", (_req, res) => {
@@ -56,5 +30,4 @@ app.get("/", (_req, res) => {
 
 // Start the server at port
 app.listen(4000);
-console.log(`Running a GraphQL API server at http://localhost:4000/graphql
-Running a GraphQL API server at http://localhost:4000/graphql?query={hello,age,weight,isOver18,hobbies,quoteOfTheDay}`);
+console.log(`Running a GraphQL API server at http://localhost:4000/graphql`);
